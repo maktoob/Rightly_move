00b25a92c8bf3d166b7b766b31742057ca1daf16
JW0419 <59473107+JW0419@users.noreply.github.com>
2021-01-13 14:36:57 +0000

    Add models - Post, Category

--- website/models.py
+++ website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    topic = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('topic', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.topic
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    category = models.ManyToManyField(Category, related_name='posts')
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
