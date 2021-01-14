1ff8dc3909f7b04c2a0b45ceab19168a88606855
JW0419 <59473107+JW0419@users.noreply.github.com>
2021-01-14 12:26:10 +0000

    Config post detail view

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from . import views
 from .views import home
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
