a96108cb66aceb1ddc1fdc9d59fcbe87ea8b9a7a
JW0419 <59473107+JW0419@users.noreply.github.com>
2021-01-15 10:54:50 +0000

    Add and config comments form

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import *
+from .forms import CommentForm
 
 
 def home(request):
@@ -14,8 +15,24 @@ def home(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     categories = post.category.all()
+    comments = post.comments.all()
+    new_comment = post.comments.all()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            # new_comment.active = True
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
     context = {'post': post,
                'categories': categories,
+               'new_comment': new_comment,
+               'comment_form': comment_form,
                }
     return render(request, 'website/post_detail.html', context=context)
 
