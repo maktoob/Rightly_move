ad8c2e6a67f6f465f38332fe74d5b0f04cd0161b
JW0419 <59473107+JW0419@users.noreply.github.com>
2021-01-19 13:18:54 +0000

    Add search function and search result page

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ from .views import home
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('search/', views.search_result, name='search_result'),
     path('<category>/', views.post_by_category, name='post_by_category'),
 ]
