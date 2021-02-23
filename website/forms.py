2e782897cb80319b120faba3e28127da4a8b0523
JW0419 <59473107+JW0419@users.noreply.github.com>
2021-02-23 15:53:36 +0000

    Config contact page and add contact form

--- website/forms.py
+++ website/forms.py
@@ -6,3 +6,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'text')
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
