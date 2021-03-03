bdb4991457dc3990b4e533ac61b508f3674a3f7e
JW0419 <59473107+JW0419@users.noreply.github.com>
2021-03-03 21:36:30 +0000

    Fix postgres config

--- rightlymove/settings/production.py
+++ rightlymove/settings/production.py
@@ -3,7 +3,5 @@ import dj_database_url
 from .base import *
 
 DATABASES = {
-    {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
